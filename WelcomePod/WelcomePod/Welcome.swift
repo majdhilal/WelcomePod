4f5b5e047e8b6e57451b986c39f8ea6351a33f44
Majd <majdhilalofficial@gmail.com>
2024-09-25 13:21:08 +0300

    first pod publish

---
+++ WelcomePod/WelcomePod/Welcome.swift
@@ -0,0 +1,15 @@
+//
+//  Welcome.swift
+//  WelcomePod
+//
+//  Created by Majd Hilal on 9/25/24.
+//
+
+import Foundation
+
+public struct Welcome {
+    
+    public static func printWelcome() {
+        print("Welcome to Xcode Project!")
+    }
+}
---
+++ WelcomePod/WelcomePod/WelcomePod.h
@@ -0,0 +1,18 @@
+//
+//  WelcomePod.h
+//  WelcomePod
+//
+//  Created by Majd Hilal on 9/25/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WelcomePod.
+FOUNDATION_EXPORT double WelcomePodVersionNumber;
+
+//! Project version string for WelcomePod.
+FOUNDATION_EXPORT const unsigned char WelcomePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WelcomePod/PublicHeader.h>
+
+
